c4a78052ff6c5326fe440d1930b913b5d68e09dd
Nick Russo <nick@cs.uchicago.edu>
2012-02-21 15:19:50 -0600

    more consistent variable names

--- heat_3D.h
+++ heat_3D.h
@@ -15,13 +15,13 @@ typedef struct {
   enum nummethod method;
 } prefs3D;
 
-void solve(const prefs3D *p, double Cx, double Cy, double Cz, double(*f)(double,double,double));
+void solve(const prefs3D *p, double Cx, double Cy, double Cz, double(*init)(double,double,double));
 void ftcs(const prefs3D *p, temp3D *d, temp3D *s, double Cx, double Cy, double Cz);
 void cn(const prefs3D *p, temp3D *d, temp3D *s, double **A, double Cx, double Cy, double Cz);
 void becs(temp3D *d, temp3D *s, double **A);
 void populate_becs_matrix(const prefs3D *p, double **A, long X, long Y, long Z, double Cx, double Cy, double Cz);
 void set_constant_boundary(const prefs3D *p, temp3D *t);
-void set_initial_with_noise(const prefs3D *p, temp3D *t, double *x, double *y, double *z, double(*f)(double,double,double));
+void set_initial_with_noise(const prefs3D *p, temp3D *t, double *x, double *y, double *z, double(*init)(double,double,double));
 double gauss3(double x, double y, double z);
 
 #endif
