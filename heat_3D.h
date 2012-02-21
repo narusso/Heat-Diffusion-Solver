44a6e8ae7f31b02087b4723a5da7a2de8717e253
Nick Russo <nick@cs.uchicago.edu>
2012-02-21 15:16:13 -0600

    shorten dimension variable names

--- heat_3D.h
+++ heat_3D.h
@@ -19,7 +19,7 @@ void solve(const prefs3D *p, double Cx, double Cy, double Cz, double(*f)(double,
 void ftcs(const prefs3D *p, temp3D *d, temp3D *s, double Cx, double Cy, double Cz);
 void cn(const prefs3D *p, temp3D *d, temp3D *s, double **A, double Cx, double Cy, double Cz);
 void becs(temp3D *d, temp3D *s, double **A);
-void populate_becs_matrix(const prefs3D *p, double **A, long Xdim, long Ydim, long Zdim, double Cx, double Cy, double Cz);
+void populate_becs_matrix(const prefs3D *p, double **A, long X, long Y, long Z, double Cx, double Cy, double Cz);
 void set_constant_boundary(const prefs3D *p, temp3D *t);
 void set_initial_with_noise(const prefs3D *p, temp3D *t, double *x, double *y, double *z, double(*f)(double,double,double));
 double gauss3(double x, double y, double z);
