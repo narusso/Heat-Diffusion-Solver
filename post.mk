32b40bc0cb4d812db5ff4e127d689817b08b1bb5
Nick Russo <nick@cs.uchicago.edu>
2012-02-24 23:03:56 -0600

    different problem sizes for implicit and explicit methods

--- post.mk
+++ post.mk
@@ -1,9 +1,14 @@
-flatten: flatten.o nrutil.o gauss_elim.o 
+flatten: flatten.o nrutil.o gauss_elim.o
 FTCS_1D: FTCS_1D.o nrutil.o
 FTCS_2D: FTCS_2D.o nrutil.o
 FTCS_3D: FTCS_3D.o nrutil.o
-heat_2D: heat_2D.o nrutil.o gauss_elim.o 
+heat_2D: heat_2D.o nrutil.o gauss_elim.o
 main: main.o heat_3D.o nrutil.o gauss_elim.o utilities.o
-timing: main
-	for M in FTCS CN BE; do \
-	 for D in `seq 2 4 14`; do time ./main -q -X1 -Y1 -Z1 -x$$D -y$$D -z$$D -n4 -s1 -p0 -t.01 -r0 -b0 -m$$M -O$${M}_$${D}_perf.dat; done; done
+CMD=./main -q -X1 -Y1 -Z1 -n3 -s1 -p0 -t.01 -r0 -b0
+timing: timing_ex timing_im
+timing_ex: main
+	for M in FTCS; do \
+	 for D in `seq 5 1 49`; do $(CMD) -x$$D -y$$D -z$$D -m$$M -Otimings/$${M}_$${D}_perf.dat; done; done
+timing_im: main
+	for M in CN BE; do \
+	 for D in `seq 5 1 15`; do $(CMD) -x$$D -y$$D -z$$D -m$$M -Otimings/$${M}_$${D}_perf.dat; done; done
