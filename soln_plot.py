77a04b1b8a08e94f8ead02b5ab8b75e4a2a04f38
Nick Russo <nick@cs.uchicago.edu>
2012-02-26 14:15:05 -0600

    plot data from -o flag

---
+++ soln_plot.py
@@ -0,0 +1,22 @@
+#!/opt/epd/bin/python
+
+import scipy, numpy
+import os, sys, re
+import matplotlib.pyplot as plt
+
+y = scipy.loadtxt("soln.dat")
+timesteps = len(numpy.unique(y[:, 0]))
+dx = len(numpy.unique(y[:, 1]))
+dy = len(numpy.unique(y[:, 2]))
+dz = len(numpy.unique(y[:, 3]))
+y = y[:, 4] # grab temp data
+y = scipy.resize(y, [timesteps, dx, dy, dz])
+
+plt.clf()
+
+rows=2; cols=4
+for i in range(rows*cols):
+  plt.subplot(rows,cols,i+1)
+  plt.imshow(y[i*timesteps/(rows*cols), :, :, dz/2]); plt.colorbar()
+
+plt.show()
