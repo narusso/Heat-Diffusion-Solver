6eeceea2f33be2714c617ae8266a34da9700af72
Nick Russo <nick@cs.uchicago.edu>
2012-03-09 00:40:00 -0600

    mglin code, slightly tweaked

---
+++ mglin/main.c
@@ -0,0 +1,25 @@
+#include "mg.h"
+#include "nrutil.h"
+#include <stdio.h>
+int main(int argc, char **argv){
+  int i,j;
+  FILE *outfile;
+  double **f;
+  int n=33;
+  int ncycle=2;
+  f = dmatrix(1,n,1,n);
+  f[n/2+1][n/2+1]=-1000.0;
+  //  for (i=2;i<n;++i)
+  //  for (j=2;j<n;++j)
+  //    f[i][j] = 2.0;
+  H("mglin:");
+  mglin(f,n,ncycle);
+  H("fopen:");
+  outfile = fopen("soln.dat", "w");
+  fwrite(&f[1][1],sizeof(double),n*n,outfile);
+  fclose(outfile);
+}
+
+void here(const char *s, const char *file, int line, const char *func){
+  fprintf(stderr, "@ %s:%d in %s: %s\n", file, line, func, s);
+}
